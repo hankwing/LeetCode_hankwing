2bb5d0dcbd33d4ed610dea38eb171318761b8347
hankwing <hankwing@hotmail.com>
2016-12-14 09:04:04 +0800

    Add 416

--- src/Problem416.cpp
+++ src/Problem416.cpp
@@ -16,6 +16,7 @@
 #include <iterator>
 #include <set>
 #include <limits.h>
+#include <bitset>
 
 using namespace std;
 
@@ -29,26 +30,16 @@ public:
         }
         // 如果sum为奇数 则一定false
         if( sum & 1 == 1) return false;
+        bitset<5001> bits(1);
+		for (int i = 0; i < nums.size(); i++) bits |= bits << nums[i];
+		return !(sum & 1) && bits[sum >> 1];
 
-        return helper(nums, 0, sum >> 1);
-
-    }
-
-    // subsum的问题
-    bool helper( vector<int>& nums, int start, int target) {
-        if(target == 0) return true;
-        for( int i = start; i < nums.size(); i++) {
-
-            if( nums[i] <= target && (helper( nums, i ,target - nums[i]) == true)) return true;
-        }
-
-        return false;
     }
 };
 
 int main( void) {
 	Solution a;
-	static const int arr[] = {1,2,5};
+	static const int arr[] = {1,1};
 	vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]) );
 	a.canPartition(vec);
 	return 0;
